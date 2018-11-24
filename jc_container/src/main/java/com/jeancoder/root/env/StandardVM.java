59077747fe0f6f65251b0b1e964027e22d381dc9
jackielee <jackielee@192.168.1.10>
2018-11-24 16:35:25 +0800

    f

--- jc_container/src/main/java/com/jeancoder/root/env/StandardVM.java
+++ jc_container/src/main/java/com/jeancoder/root/env/StandardVM.java
@@ -3,6 +3,7 @@ package com.jeancoder.root.env;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.jeancoder.root.container.loader.BootClassLoader;
 import com.jeancoder.root.vm.DefaultVm;
 import com.jeancoder.root.vm.JCVM;
 
@@ -11,6 +12,8 @@ public class StandardVM extends DefaultVm implements JCVM {
 	private static Logger logger = LoggerFactory.getLogger(StandardVM.class);
 	
 	private final static StandardVM INSTANCE = new StandardVM();
+	
+	private BootClassLoader rootLoader = null;
 	
 	public static StandardVM getVM() {
 		synchronized (StandardVM.class) {
@@ -34,9 +37,14 @@ public class StandardVM extends DefaultVm implements JCVM {
 	public synchronized void onStart() {
 		logger.info("starting......");
 		state = STATE_STARTING;
+		//准备基础环境
+		rootLoader = new BootClassLoader(Thread.currentThread().getContextClassLoader());
 		if(this.appList!=null) {
 			for(JCAPP jca : this.appList) {
 				BootContainer bc = new BootContainer(jca);
+				bc.bindBaseEnv(rootLoader);
+				bc.onInit();
+				bc.onStart();
 				if(bc.state().equals(STATE_RUNNING)) {
 					VM_CONTAINERS.put(bc.id(), bc);
 				} else {
