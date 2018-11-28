8631c232d62f1a093d7b46043f665c26bfc8c520
jackielee <jackielee@192.168.1.10>
2018-11-28 15:13:17 +0800

    f

---
+++ jc_server/src/main/java/com/jeancoder/root/server/mixture/ByteResults.java
@@ -0,0 +1,27 @@
+package com.jeancoder.root.server.mixture;
+
+public class ByteResults {
+
+	private byte[] results;
+	private String status;
+	private String msg;
+	
+	public byte[] getResults() {
+		return results;
+	}
+	public void setResults(byte[] results) {
+		this.results = results;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
---
+++ jc_server/src/main/java/com/jeancoder/root/server/util/ZipUtil.java
@@ -0,0 +1,52 @@
+package com.jeancoder.root.server.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import com.jeancoder.core.util.FileUtil;
+
+public class ZipUtil {
+	
+	public static void  unzip(String path, ZipInputStream inputStream) throws Exception {
+		ZipEntry entry = inputStream.getNextEntry();
+		String name = entry.getName();
+		File appFile = new File(path);
+		if (appFile.exists()) {
+			FileUtil.deletefile(appFile);
+		}
+		appFile.mkdir();
+//		entry = inputStream.getNextEntry();
+		while (((entry = inputStream.getNextEntry()) != null)) {
+			String entryName = entry.getName();
+			entryName = entryName.substring(name.length(),entryName.length());
+			File file = new File(path + File.separator + entryName);
+			if (entry.isDirectory()) {
+				file.mkdirs();
+			} else {
+				if(!file.getParentFile().exists()) {
+					file.getParentFile().mkdirs();
+				}
+				file.createNewFile();
+				OutputStream out = new FileOutputStream(file);
+				int len;
+				byte[] _byte = new byte[1024];
+				while ((len = inputStream.read(_byte)) > 0) {
+					out.write(_byte, 0, len);
+				}
+				out.close();
+			}
+		}
+	}
+	
+	public static  void main(String[] arg) {
+//        File file_base=new File("E:\\hello.zip");
+		String entryName = "1231/123123/124124";
+		String name = "1231/";
+		entryName = entryName.substring(name.length(),entryName.length());
+		System.out.println(entryName);
+	}
+}
