186a932d02c36f8a27a47f7cf9ee267f4e458455
jackielee <jackielee@192.168.1.10>
2018-11-30 18:55:07 +0800

    f

--- jeancoder_sdk/src/main/java/com/jc/channel/SlaveCli.java
+++ jeancoder_sdk/src/main/java/com/jc/channel/SlaveCli.java
@@ -3,6 +3,8 @@ package com.jc.channel;
 import java.util.Enumeration;
 
 import com.jc.proto.conf.AppMod;
+import com.jc.proto.msg.ct.InstallMsg;
+import com.jc.proto.msg.ct.UninstallMsg;
 import com.jc.proto.msg.ct.UpgradeMsg;
 import com.jc.shell.JCShellFac;
 import com.jc.shell.ShellServer;
@@ -25,6 +27,29 @@ public class SlaveCli {
 		UpgradeMsg msg = new UpgradeMsg(am);
 		
 		SocketChannel channel = (SocketChannel)JCShellFac.instance().dispatchaim(client_instance_id);
+		if(channel==null) {
+			throw new RuntimeException(client_instance_id + " server not connected.");
+		}
+		channel.writeAndFlush(msg);
+	}
+	
+	public void installApp(String client_instance_id, AppMod am) {
+		InstallMsg msg = new InstallMsg(am);
+		
+		SocketChannel channel = (SocketChannel)JCShellFac.instance().dispatchaim(client_instance_id);
+		if(channel==null) {
+			throw new RuntimeException(client_instance_id + " server not connected.");
+		}
+		channel.writeAndFlush(msg);
+	}
+	
+	public void uninstallApp(String client_instance_id, AppMod am) {
+		UninstallMsg msg = new UninstallMsg(am);
+		
+		SocketChannel channel = (SocketChannel)JCShellFac.instance().dispatchaim(client_instance_id);
+		if(channel==null) {
+			throw new RuntimeException(client_instance_id + " server not connected.");
+		}
 		channel.writeAndFlush(msg);
 	}
 }
