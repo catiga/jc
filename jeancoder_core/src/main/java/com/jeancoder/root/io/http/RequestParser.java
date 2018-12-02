21622dfd314c304b36cf7941243b4049284af745
jackielee <jackielee@192.168.1.10>
2018-12-02 14:13:57 +0800

    fix parameters of request uri bugs

--- jeancoder_core/src/main/java/com/jeancoder/root/io/http/RequestParser.java
+++ jeancoder_core/src/main/java/com/jeancoder/root/io/http/RequestParser.java
@@ -15,7 +15,6 @@ import org.slf4j.LoggerFactory;
 
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
-import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.handler.codec.http.multipart.Attribute;
 import io.netty.handler.codec.http.multipart.FileUpload;
 import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
@@ -28,6 +27,32 @@ public class RequestParser {
 	
 	DiskFileItemFactory factory = new DiskFileItemFactory();
 	
+	Map<String, String[]> params(String full_uri) {
+		Map<String, String[]> parameters = new HashMap<String, String[]>();
+		if(full_uri.indexOf("?")<0) {
+			return parameters;
+		}
+		String[] params = full_uri.substring(full_uri.indexOf("?") + 1).trim().split("&");
+		for(String k : params) {
+			String[] key_values = k.split("=");
+			String key = key_values[0].trim();
+			String value = "";
+			if(key_values.length>1) {
+				value = key_values[1];
+			}
+			String[] exist_values = parameters.get(key);
+			List<String> inner_list = null;
+			if(exist_values!=null&&exist_values.length>0) {
+				inner_list = Arrays.asList(exist_values);
+			} else {
+				inner_list = new ArrayList<String>();
+			}
+			inner_list.add(value);
+			parameters.put(key, inner_list.toArray(new String[inner_list.size()]));
+		}
+		return parameters;
+	}
+	
 	/**
 	 * 解析请求参数
 	 * 
@@ -37,13 +62,14 @@ public class RequestParser {
 	 * @throws IOException
 	 */
 	public ReqTotal parse(FullHttpRequest fullReq) throws IOException {
-		Map<String, String[]> parameters = new HashMap<String, String[]>();
+		String full_uri = fullReq.uri();
+		Map<String, String[]> parameters = this.params(full_uri);
 		List<UploadFile> files = new ArrayList<UploadFile>();
 		
-		new QueryStringDecoder(fullReq.uri()).parameters().entrySet().forEach(entry -> {
-			List<String> values = entry.getValue();
-			parameters.put(entry.getKey(), values.toArray(new String[values.size()]));
-		});
+//		full_params.entrySet().forEach(entry -> {
+//			List<String> values = entry.getValue();
+//			parameters.put(entry.getKey(), values.toArray(new String[values.size()]));
+//		});
 		
 		HttpMethod method = fullReq.method();
 		if (HttpMethod.POST == method) {
@@ -62,7 +88,7 @@ public class RequestParser {
 					if (fileUpload.isCompleted()) {
 						// 暂存到磁盘
 						String tmp_dir = System.getProperty("java.io.tmpdir");
-						tmp_dir = "/Users/jackielee/Desktop/test";
+						//tmp_dir = "/Users/jackielee/Desktop/test";
 						tmp_dir = tmp_dir + File.separator + "tmp" + File.separator + Thread.currentThread().getId() + File.separator + System.currentTimeMillis() + "/";
 						File parent_path = new File(tmp_dir);
 						if(!parent_path.exists()) {
