03da761e23416d33f779e660c5ac661edf685602
jayp_hj <huang_jie@cpis.cn>
2018-12-04 14:11:27 +0800

    中央服务器启动方法

--- jc_server/src/main/java/com/jeancoder/root/server/fk/Starter.java
+++ jc_server/src/main/java/com/jeancoder/root/server/fk/Starter.java
@@ -14,11 +14,14 @@ import com.jc.proto.conf.FkConf;
 import com.jc.proto.conf.ServerMod;
 import com.jeancoder.core.util.JackSonBeanMapper;
 import com.jeancoder.root.server.inet.JCServer;
+import com.jeancoder.root.server.inet.ServerCode;
 import com.jeancoder.root.server.inet.ServerFactory;
 import com.jeancoder.root.server.mixture.ByteResults;
 import com.jeancoder.root.server.state.ServerHolder;
 import com.jeancoder.root.server.util.RemoteUtil;
 import com.jeancoder.root.server.util.ZipUtil;
+import com.jeancoder.root.vm.JCVM;
+import com.jeancoder.root.vm.JCVMDelegatorGroup;
 
 public class Starter {
 
@@ -33,6 +36,8 @@ public class Starter {
 	 */
 	public static void main(String[] argc) {
 		start();
+		// 中央服务打包启动方法
+		//centralServerStart();
 	}
 
 	public static void start() {
@@ -60,7 +65,7 @@ public class Starter {
 				}
 				json = new String(byteResults.getResults());
 			} catch (Exception e) {
-				logger.error("网络加载配置文件错误",e);
+				logger.error("网络加载配置文件错误", e);
 			}
 		}
 		if (json == null) {
@@ -72,35 +77,131 @@ public class Starter {
 		try {
 			Gson gson = new Gson();
 			fk_con = gson.fromJson(json, FkConf.class);
-			if(fk_con==null||fk_con.getServers()==null||fk_con.getServers().isEmpty()) {
+			if (fk_con == null || fk_con.getServers() == null || fk_con.getServers().isEmpty()) {
 				throw new RuntimeException("EMPTY");
 			}
 		} catch (Exception e) {
 			logger.error("start error:", e);
 			System.exit(-1);
 		}
-			for (ServerMod sm : fk_con.getServers()) {
-				JCServer server = ServerFactory.generate(sm);
-				ServerHolder.getHolder().add(server);
-				new Thread(new Runnable() {
+		for (ServerMod sm : fk_con.getServers()) {
+			JCServer server = ServerFactory.generate(sm);
+			ServerHolder.getHolder().add(server);
+			new Thread(new Runnable() {
 
-					@Override
-					public void run() {
-						for (AppMod mod : sm.getApps()) {
-							try {
-								if (mod.getFetch_address() != null) {
-									System.out.println("开始下载");
-									InputStream ins = RemoteUtil.installation(mod.getFetch_address(),new Long(mod.getApp_id()));
-									System.out.println("下载");
+				@Override
+				public void run() {
+					for (AppMod mod : sm.getApps()) {
+						try {
+							if (mod.getFetch_address() != null) {
+								System.out.println("开始下载");
+								InputStream ins = RemoteUtil.installation(mod.getFetch_address(),
+										new Long(mod.getApp_id()));
+								System.out.println("下载");
+								ZipUtil.unzip(mod.getApp_base(), new ZipInputStream(ins));
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+					server.start();
+				}
+			}).start();
+		}
+	}
+
+	/**
+	 * 中央服务器启动测试环境å
+	 */
+	public static void centralServerStart() {
+		String json = null;
+		try {
+			// 本地读取配置文件
+			InputStream ins = Starter.class.getClassLoader().getResourceAsStream(appConf);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(ins));
+
+			String lineContent = null;
+			StringBuffer buff = new StringBuffer();
+			while ((lineContent = reader.readLine()) != null) {
+				buff.append(lineContent);
+			}
+			json = buff.toString();
+		} catch (Exception e) {
+		}
+		if (json == null) {
+			logger.error("配置文件不存在");
+			System.exit(-1);
+			return;
+		}
+		FkConf fk_con = null;
+		try {
+			Gson gson = new Gson();
+			fk_con = gson.fromJson(json, FkConf.class);
+			if (fk_con == null || fk_con.getServers() == null || fk_con.getServers().isEmpty()) {
+				throw new RuntimeException("EMPTY");
+			}
+		} catch (Exception e) {
+			logger.error("start error:", e);
+			System.exit(-1);
+		}
+		for (ServerMod sm : fk_con.getServers()) {
+			JCServer server = ServerFactory.generate(sm);
+			ServerHolder.getHolder().add(server);
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					server.start();
+				}
+			}).start();
+		}
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				logger.error("线程启动");
+				try {
+					Thread.sleep(15000);
+					String json = "";
+					try {
+						String rules = RemoteUtil.getConfigList();
+						ByteResults byteResults = JackSonBeanMapper.fromJson(rules, ByteResults.class);
+						if (!"0000".equals(byteResults.getStatus())) {
+							logger.info("获取配置失败 status:" + byteResults.getStatus() + " msg:" + byteResults.getMsg());
+							return;
+						}
+						json = new String(byteResults.getResults());
+					} catch (Exception e) {
+						logger.error("网络加载配置文件错误", e);
+					}
+					try {
+						Gson gson = new Gson();
+						FkConf fk_con = gson.fromJson(json, FkConf.class);
+						for (ServerMod sm : fk_con.getServers()) {
+							if (!sm.getScheme().equalsIgnoreCase(ServerCode.HTTP.toString())) {
+								continue;
+							}
+							for (AppMod mod : sm.getApps()) {
+								try {
+									InputStream ins = RemoteUtil.installation(mod.getFetch_address(),
+											new Long(mod.getApp_id()));
 									ZipUtil.unzip(mod.getApp_base(), new ZipInputStream(ins));
+									JCVM jcvm = JCVMDelegatorGroup.instance().getDelegator().getVM();
+									jcvm.installApp(mod.to());
+								} catch (Exception e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
 								}
-							} catch (Exception e) {
-								e.printStackTrace();
 							}
 						}
-						server.start();
+					} catch (Exception e) {
+						logger.error("start error:", e);
+						System.exit(-1);
 					}
-				}).start();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
+		}).start();
 	}
 }
