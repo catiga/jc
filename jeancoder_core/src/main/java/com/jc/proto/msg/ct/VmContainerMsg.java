c277e870d2f9dfd7dc8f695c37418fb137a08967
jackielee <jackielee@bogon>
2018-12-09 18:51:22 +0800

    f

--- jeancoder_core/src/main/java/com/jc/proto/msg/ct/VmContainerMsg.java
+++ jeancoder_core/src/main/java/com/jc/proto/msg/ct/VmContainerMsg.java
@@ -1,23 +1,27 @@
 package com.jc.proto.msg.ct;
 
+import java.util.Map;
+
 import com.jc.proto.msg.GeneralMsg;
+import com.jeancoder.root.bean.ContainerBean;
 import com.jeancoder.root.container.ContainerMaps;
+import com.jeancoder.root.container.core.BCID;
 
 @SuppressWarnings("serial")
 public class VmContainerMsg extends GeneralMsg {
 	
-	ContainerMaps conthos;
+	Map<BCID, ContainerBean> conthos;
 
-	public ContainerMaps getConthos() {
+	public Map<BCID, ContainerBean> getConthos() {
 		return conthos;
 	}
 
-	public void setConthos(ContainerMaps conthos) {
+	public void setConthos(Map<BCID, ContainerBean> conthos) {
 		this.conthos = conthos;
 	}
 	
 	public VmContainerMsg(ContainerMaps hos) {
 		super();
-		this.conthos = hos;
+		this.conthos = ContainerBean.cons(hos);
 	}
 }
