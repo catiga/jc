d8e53a40caa8309dfb686661ea8fff62b0e0de29
jackielee <jackielee@192.168.1.24>
2018-12-11 10:33:13 +0800

    f

--- jeancoder_core/src/main/java/com/jc/proto/msg/qd/SelectHandler.java
+++ jeancoder_core/src/main/java/com/jc/proto/msg/qd/SelectHandler.java
@@ -4,10 +4,20 @@ import com.jc.proto.msg.MsgType;
 
 @SuppressWarnings("serial")
 public class SelectHandler extends DataHandler {
+	
+	private String sql;
 
 	public SelectHandler(String insid, String cont_id, String cont_code) {
 		super(insid, cont_id, cont_code);
 		setType(MsgType.HANDLER_SELECT);
+	}
+
+	public String getSql() {
+		return sql;
+	}
+
+	public void setSql(String sql) {
+		this.sql = sql;
 	}
 	
 }
