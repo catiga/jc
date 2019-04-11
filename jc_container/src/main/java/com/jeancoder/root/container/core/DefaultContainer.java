1fafce313794eecb2847a57974fb92429c4b09b6
jackielee <jackielee@192.168.100.231>
2019-04-11 19:18:42 +0800

    p

--- jc_container/src/main/java/com/jeancoder/root/container/core/DefaultContainer.java
+++ jc_container/src/main/java/com/jeancoder/root/container/core/DefaultContainer.java
@@ -289,8 +289,8 @@ public abstract class DefaultContainer extends QContainer implements JCAppContai
 	
 	@Override
 	public final <T extends Result> RunnerResult<T> callEntry(JCHttpRequest req, JCHttpResponse res) {
-//		String servlet_path = req.getRequestURI().substring(("/" + appins.getCode()).length());
 		String servlet_path = req.getPathInfo();
+		logger.info("servlet_path======" + servlet_path + ", and appins===" + (appins==null));
 		if(servlet_path.startsWith("/" + appins.getSta_base())) {
 			//说明是静态资源
 			Result result = new Result().setStaticName(servlet_path.substring(("/" + appins.getSta_base()).length() + 1));
