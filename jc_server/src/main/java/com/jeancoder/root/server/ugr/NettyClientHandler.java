de9f2e3874c6dc49b55abd9436de38a3e45e80b8
jackielee <jackielee@192.168.100.231>
2019-04-12 01:04:21 +0800

    f

--- jc_server/src/main/java/com/jeancoder/root/server/ugr/NettyClientHandler.java
+++ jc_server/src/main/java/com/jeancoder/root/server/ugr/NettyClientHandler.java
@@ -179,7 +179,7 @@ public class NettyClientHandler extends SimpleChannelInboundHandler<GeneralMsg> 
 			break;
 			
 		case INSPARAD: {
-			logger.debug("accepted params settings:" + JackSonBeanMapper.toJson(baseMsg));
+			logger.info("accepted params settings:" + JackSonBeanMapper.toJson(baseMsg));
 			if(baseMsg!=null && (baseMsg instanceof ParamHandler)) {
 				ParamHandler msg = (ParamHandler)baseMsg;
 				ParamMod mod = msg.getParams();
