1f7c87ad8950e8bf1bddbfee5fda62f587c9fc24
jackielee <jackielee@192.168.0.101>
2019-06-01 10:44:34 +0800

    修改注释

--- jeancoder_core/src/main/java/com/jc/proto/msg/GeneralMsg.java
+++ jeancoder_core/src/main/java/com/jc/proto/msg/GeneralMsg.java
@@ -15,7 +15,7 @@ public abstract class GeneralMsg  implements MsgProto, Serializable {
     
     private MsgType type;
     
-    //必须唯一，否者会出现channel调用混乱
+    //必须唯一，否者会出现channel调用混乱，clientId实际即为instance id，即实例ID
     private String clientId;
     
     //初始化客户端id和消息全局ID
