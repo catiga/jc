59f92138a86ae2815adfd6e89aea8aff8319fde8
jackielee <jackielee@192.168.0.101>
2019-06-02 14:04:34 +0800

    f

--- jeancoder_core/src/main/java/com/jeancoder/root/container/ContainerMaps.java
+++ jeancoder_core/src/main/java/com/jeancoder/root/container/ContainerMaps.java
@@ -1,18 +1,24 @@
 package com.jeancoder.root.container;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
-import java.util.concurrent.ConcurrentHashMap;
 
 import com.jeancoder.root.container.core.BCID;
 
 @SuppressWarnings("serial")
 public class ContainerMaps implements Serializable {
 
-	private final Map<BCID, JCAppContainer> VM_CONTAINERS = new ConcurrentHashMap<BCID, JCAppContainer>();
+	//private final Map<BCID, JCAppContainer> VM_CONTAINERS = new ConcurrentHashMap<BCID, JCAppContainer>();
+	
+	private final Map<BCID, JCAppContainer> VM_CONTAINERS = Collections.synchronizedMap(new IdentityHashMap<BCID, JCAppContainer>());
 	
 	public Set<BCID> keySet() {
 		return VM_CONTAINERS.keySet();
@@ -38,6 +44,23 @@ public class ContainerMaps implements Serializable {
 	
 	public void remove(BCID id) {
 		VM_CONTAINERS.remove(id);
+	}
+	
+	public void destroy(BCID newid) {
+		Iterator<BCID> exist_its = VM_CONTAINERS.keySet().iterator();
+		List<BCID> need_remove_bcids = new ArrayList<BCID>();
+		while(exist_its.hasNext()) {
+			BCID exist_bcid = exist_its.next();
+			if(exist_bcid!=newid && exist_bcid.code().equals(newid.code())) {
+				need_remove_bcids.add(exist_bcid);
+			}
+		}
+		if(!need_remove_bcids.isEmpty()) {
+			for(BCID bcid : need_remove_bcids) {
+				VM_CONTAINERS.get(bcid).onDestroy();
+				VM_CONTAINERS.remove(bcid);
+			}
+		}
 	}
 	
 //	public void shutdown() {
--- jeancoder_core/src/main/java/com/jeancoder/root/container/core/BCID.java
+++ jeancoder_core/src/main/java/com/jeancoder/root/container/core/BCID.java
@@ -33,13 +33,13 @@ public class BCID implements Serializable {
 		this.code = code;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((id == null) ? 0 : id.hashCode());
+//		return result;
+//	}
 
 	@Override
 	public boolean equals(Object obj) {
