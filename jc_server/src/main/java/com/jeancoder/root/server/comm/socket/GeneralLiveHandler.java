016531bd3343e6f55d8580e9a20f8d9f17dde5ab
jackielee <jackielee@192.168.0.103>
2019-06-02 20:47:00 +0800

    需要验证正确的登录信息

--- jc_server/src/main/java/com/jeancoder/root/server/comm/socket/GeneralLiveHandler.java
+++ jc_server/src/main/java/com/jeancoder/root/server/comm/socket/GeneralLiveHandler.java
@@ -1,5 +1,12 @@
 package com.jeancoder.root.server.comm.socket;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,8 +19,14 @@ import com.jc.proto.msg.PingMsg;
 import com.jc.proto.msg.ReplyClientBody;
 import com.jc.proto.msg.ReplyMsg;
 import com.jc.proto.msg.ReplyServerBody;
+import com.jeancoder.core.power.DatabasePower;
+import com.jeancoder.core.power.result.JeancoderResultSet;
+import com.jeancoder.root.container.ContainerMaps;
+import com.jeancoder.root.container.JCAppContainer;
 import com.jeancoder.root.server.state.NettyChannelMap;
 import com.jeancoder.root.server.state.ServerHolder;
+import com.jeancoder.root.vm.JCVM;
+import com.jeancoder.root.vm.JCVMDelegatorGroup;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
@@ -58,6 +71,49 @@ public class GeneralLiveHandler extends SimpleChannelInboundHandler<MsgProto> {
 		//ctx.close();
 	}
 	
+	@SuppressWarnings("deprecation")
+	protected List<Map<String, String>> findInstanceByMerchantAndInstanceCode(String merchant_code, String instance_code) {
+		JCVM jcvm = JCVMDelegatorGroup.instance().getDelegator().getVM();
+		ContainerMaps conts = jcvm.getContainers();
+		JCAppContainer container = conts.getByCode("server").nextElement();		//这里就是找中央服务的code对应的容器
+		
+		try {
+			DatabasePower db_pow = container.getCaps().getDatabase();
+			String sql = "select id, name, ver_install_id, now_ver_id, now_ver_num from j_instance where code='" + instance_code + "' and m_id in (select id from j_merchants where merchants_code='" + merchant_code + "')";
+			JeancoderResultSet result = null;
+			try {
+				result = db_pow.doQuery(sql);
+				ResultSet rs = result.getResultSet();
+				List<Map<String, String>> tables = new LinkedList<Map<String, String>>();
+				while(rs.next()) {
+					Map<String, String> data_i = new HashMap<String, String>();
+					ResultSetMetaData metadata = rs.getMetaData();
+					int col_size = metadata.getColumnCount();
+					for(int i=1; i<=col_size; i++) {
+						String column_name = metadata.getColumnName(i);
+						String column_alias_name = metadata.getColumnLabel(i);
+						if(column_alias_name!=null) {
+							column_name = column_alias_name;
+						}
+						Object value = rs.getObject(i);
+						data_i.put(column_name, value==null?null:value.toString());
+					}
+					tables.add(data_i);
+				}
+				return tables;
+			} catch(Exception e) {
+				throw new RuntimeException(e);
+			} finally {
+				if(result!=null) {
+					result.closeConnection();
+				}
+			}
+		} catch(Exception e) {
+			logger.error("", e);
+			return null;
+		}
+	}
+	
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, MsgProto msgProto) throws Exception {
 		String version = msgProto.version();		//消息版本号
@@ -71,10 +127,57 @@ public class GeneralLiveHandler extends SimpleChannelInboundHandler<MsgProto> {
 		logger.debug("server receive message=" + baseMsg + " and from client_id:::" + baseMsg.getClientId());
 		if (MsgType.LOGIN.equals(baseMsg.getType())) {
 			LoginMsg loginMsg = (LoginMsg) baseMsg;
-			if ("jclogin".equals(loginMsg.getUserName()) && "jcpass".equals(loginMsg.getPassword())) {	//暂存的处理方式
-				// 登录成功,把channel存到服务端的map中
-				NettyChannelMap.add(loginMsg.getClientId(), (SocketChannel) ctx.channel());
+			String user_name = loginMsg.getUserName();	//这里应该是 merchant_code
+			String password = loginMsg.getPassword();	//这里应该是 instance_code
+			
+			List<Map<String, String>> instance_data = this.findInstanceByMerchantAndInstanceCode(user_name, password);
+			SocketChannel need_save_channel = null;
+			if(instance_data!=null) {
+				if(instance_data.size()>1) {
+					//说明配置错误，需要打印日志并提醒
+					logger.error("LOGIN_CONFIG_ERROR:client_id:" + instance_id + " and merchant_code=" + user_name + " and instance_code=" + password + " was repeated, please check...");
+				} else {
+					Map<String, String> instance_obj = instance_data.get(0);
+					String instance_obj_id = instance_obj.get("id").toString();			// 一定不是null
+					String instance_obj_name = instance_obj.get("name")==null ? null : instance_obj.get("name").toString();
+					String instance_obj_ver_install_id = instance_obj.get("ver_install_id")==null ? null : instance_obj.get("ver_install_id").toString();
+					String instance_obj_now_ver_id = instance_obj.get("now_ver_id")==null ? null : instance_obj.get("now_ver_id").toString();
+					String instance_obj_now_ver_num = instance_obj.get("now_ver_num")==null ? null : instance_obj.get("now_ver_num").toString();
+					
+					logger.info("LOGIN MSG>>>");
+					logger.info("instance_obj_id=" + instance_obj_id + ", instance_obj_name=" + instance_obj_name 
+							+ ", instance_obj_ver_install_id=" + instance_obj_ver_install_id
+							+ ", instance_obj_now_ver_id=" + instance_obj_now_ver_id
+							+ ", instance_obj_now_ver_num=" + instance_obj_now_ver_num);
+					if(instance_obj_id.equals(instance_id)) {
+						//成功匹配到
+						need_save_channel = (SocketChannel)ctx.channel();
+					} else {
+						logger.error("instance_id=" + instance_obj_id + " login failed and invalid request from IP:" + ctx.channel().remoteAddress());
+					}
+				}
+			} else {
+				logger.error("merchant_code=" + user_name + " and instance_code=" + password + " was not found, try the old version code.");
+				//旧的连接方式
+				if ("jclogin".equals(user_name) && "jcpass".equals(password)) {	//暂存的处理方式
+					// 登录成功,把channel存到服务端的map中
+//					NettyChannelMap.add(loginMsg.getClientId(), (SocketChannel) ctx.channel());
+//					logger.info("client" + loginMsg.getClientId() + " 登录成功");
+					
+					//成功匹配到旧版本
+					need_save_channel = (SocketChannel)ctx.channel();
+				} else {
+					//判断一下是不是从腾讯视频过来的连接
+					if(instance_id.equals("7") || instance_id.equals("8") || instance_id.equals("2")) {
+						need_save_channel = (SocketChannel)ctx.channel();
+					}
+				}
+			}
+			if(need_save_channel!=null) {
+				NettyChannelMap.add(loginMsg.getClientId(), need_save_channel);
 				logger.info("client" + loginMsg.getClientId() + " 登录成功");
+			} else {
+				ctx.channel().close();
 			}
 		} else if (MsgType.PING.equals(baseMsg.getType())) {
 			PingMsg pingMsg = (PingMsg)baseMsg;
@@ -82,13 +185,23 @@ public class GeneralLiveHandler extends SimpleChannelInboundHandler<MsgProto> {
 				//execute update channel 
 				String channel_client_id = pingMsg.getClientId();
 				Channel exist_channel = NettyChannelMap.get(channel_client_id);
-				if(exist_channel==null || (exist_channel!=ctx.channel())) {
-					logger.info("channel_client_id:::" + channel_client_id + " will be updated for The exist channel:::" + exist_channel + " is not equle the new one:::" + ctx.channel());
+				if(exist_channel!=null) {
+					if(exist_channel!=ctx.channel()) {
+						logger.info("channel_client_id:::" + channel_client_id + " will be updated for The exist channel:::" + exist_channel + " is not equle the new one:::" + ctx.channel());
+					}
+					NettyChannelMap.add(pingMsg.getClientId(), (SocketChannel) ctx.channel());
+					PingMsg replyPing = new PingMsg();
+					NettyChannelMap.get(pingMsg.getClientId()).writeAndFlush(replyPing);
+				} else {
+					logger.info("channel_client_id:::" + channel_client_id + " does not exist, invalid connect will be dropped");
+					ctx.channel().close();
 				}
-				NettyChannelMap.add(pingMsg.getClientId(), (SocketChannel) ctx.channel());
-				//reply ping msg as heart beat pack
-				PingMsg replyPing = new PingMsg();
-				NettyChannelMap.get(pingMsg.getClientId()).writeAndFlush(replyPing);
+//				if(exist_channel==null || (exist_channel!=ctx.channel())) {
+//					logger.info("channel_client_id:::" + channel_client_id + " will be updated for The exist channel:::" + exist_channel + " is not equle the new one:::" + ctx.channel());
+//				}
+//				NettyChannelMap.add(pingMsg.getClientId(), (SocketChannel) ctx.channel());
+//				PingMsg replyPing = new PingMsg();
+//				NettyChannelMap.get(pingMsg.getClientId()).writeAndFlush(replyPing);
 			}
 		} else {
 //			if (NettyChannelMap.get(baseMsg.getClientId()) == null) {
@@ -136,7 +249,7 @@ public class GeneralLiveHandler extends SimpleChannelInboundHandler<MsgProto> {
 				break;
 			}
 		}
-		
+
 		ReferenceCountUtil.release(baseMsg);
 	}
 	
