c75e8905f4edcd2b109445d17b20b04cb23af727
ttl1.rven <ttl1.rven@sgpconsult.com>
2025-04-21 20:37:18 +0800

    1

--- jc_server/src/main/java/com/jeancoder/root/server/util/RemoteUtil.java
+++ jc_server/src/main/java/com/jeancoder/root/server/util/RemoteUtil.java
@@ -20,8 +20,8 @@ public class RemoteUtil {
 	
 	private static String singkey = null; 
 	
-//	private static final String domain = "http://jcloudapp.fancynote.vip";
-	private static final String domain = "http://e.local:8081";
+	private static final String domain = "http://jcloudapp.fancynote.vip";
+//	private static final String domain = "http://e.local:8081";
 
 	private static String convertJCSDomain(String jcsDomain) {
 		if (jcsDomain == null) {
